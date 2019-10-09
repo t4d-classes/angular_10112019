ac70902d6bf053eb577aea3e4cd19b087121297e
Eric Greene <eric@t4d.io>
2019-10-09 07:00:46 -0700

    add more unit tests

--- car-tool-app/src/app/car-tool/services/cars.service.spec.ts
+++ car-tool-app/src/app/car-tool/services/cars.service.spec.ts
@@ -1,16 +1,76 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { ICar } from '../models/ICar';
 import { CarsService } from './cars.service';
 
 describe('CarsService', () => {
-  
+
+  let carsSvc: CarsService;
+  let http: HttpTestingController;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [ HttpClientTestingModule ],
+    providers: [ CarsService ],
   }));
+
+  beforeEach(() => {
+    http = TestBed.get(HttpTestingController);
+    carsSvc = TestBed.get(CarsService);
+  });
 
   it('should be created', () => {
     const service: CarsService = TestBed.get(CarsService);
     expect(service).toBeTruthy();
   });
+
+  it('should get all cars', () => {
+
+    const expectedCars = [
+      { id: 1, make: 'test', model: 'test', year: 2017, color: 'green', price: 10000 },
+      { id: 2, make: 'test', model: 'test', year: 2018, color: 'green', price: 20000 },
+    ];
+
+    carsSvc.all().subscribe(cars => {
+      expect(cars).toEqual(expectedCars);
+    });
+
+    const req = http.expectOne('http://localhost:3010/cars');
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedCars);
+
+  });
+
+  it('should get one car', () => {
+
+    const expectedCar = { id: 1, make: 'test', model: 'test', year: 2017, color: 'green', price: 10000 };
+
+    carsSvc.one(1).subscribe(cars => {
+      expect(cars).toEqual(expectedCar);
+    });
+
+    const req = http.expectOne('http://localhost:3010/cars/1');
+    expect(req.request.method).toEqual('GET');
+    req.flush(expectedCar);
+
+  });
+
+
+  it('should append a car', () => {
+
+    const carToAppend: ICar = { make: 'test', model: 'test2', year: 1900, color: 'upick', price: 1 };
+    const carAppended: ICar = { id: 1, ...carToAppend };
+
+    carsSvc.append(carToAppend).subscribe(car => expect(car).toEqual(carAppended));
+
+    const req = http.expectOne('http://localhost:3010/cars');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(carToAppend);
+    req.flush(carAppended);
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
+
 });
--- car-tool-app/src/app/shared/components/tool-footer/tool-footer.component.spec.ts
+++ car-tool-app/src/app/shared/components/tool-footer/tool-footer.component.spec.ts
@@ -29,7 +29,6 @@ describe('ToolFooterComponent', () => {
     component.termsOfUseUrl = termsOfUseUrl;
   
     fixture.detectChanges();
-  
   });
 
   it('should create', () => {
@@ -44,5 +43,49 @@ describe('ToolFooterComponent', () => {
 
     expect(spanElement.textContent).toBe(expectedSpanContent);
 
+    // tests when the privacy policy ngIf is true
+    const privacyPolicyAnchorElement = fixture.nativeElement
+      .querySelector('#privacy-policy-link') as HTMLAnchorElement;
+
+    const expectedPrivacyPolicyUrlHref = `${privacyPolicyUrl}/`;
+
+    expect(privacyPolicyAnchorElement.href).toBe(expectedPrivacyPolicyUrlHref);
+
+    const termsOfUseAnchorElement = fixture.nativeElement
+      .querySelector('#terms-of-use-link') as HTMLAnchorElement;
+
+    const expectedTermsOfUseUrlHref = `${termsOfUseUrl}/`;
+
+    expect(termsOfUseAnchorElement.href).toBe(expectedTermsOfUseUrlHref);
   });
+
+  // when an component template needs to check when the condition is both
+  // true and false
+  it('no privacy policy url', () => {
+
+    // a new fixure can be retrieved, new inputs can be specified
+    fixture = TestBed.createComponent(ToolFooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  // this test will test when ngIf on the privacy policy url is false
+    const anchorElements = fixture.nativeElement
+      .querySelectorAll('#privacy-policy-link') as NodeList;
+
+    expect(anchorElements.length).toBe(0);
+  });
+
+  it('no terms of use url', () => {
+
+    fixture = TestBed.createComponent(ToolFooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const anchorElements = fixture.nativeElement
+      .querySelectorAll('#terms-of-use-link') as NodeList;
+
+    expect(anchorElements.length).toBe(0);
+
+  });
+
 });
